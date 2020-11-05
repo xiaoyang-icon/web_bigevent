87eb87b152525e9a1e20e927dac1ba04be5f0357
hanxiaoyang <1592974490@qq.com>
2020-11-05 14:53:01 +0800

    完成了主页功能的开发

---
+++ assets/js/index.js
@@ -0,0 +1,57 @@
+$(function () {
+    //调用 getUserInfo() 获取用户信息
+    getUserInfo()
+
+    var layer = layui.layer
+    //点击退出按钮
+    $('.icon-tuichu').on('click', function () {
+        layer.confirm('确定退出登录？', { icon: 3, title: '提示' }, function (index) {
+            //1.清空本地的存储
+            localStorage.removeItem('token')
+            //2.跳转到登录页面
+            location.href = '/login.html'
+            // 关闭 confirm 询问框
+            layer.close(index)
+        })
+    })
+})
+// 获取用户的基本信息
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        // headers 就是请求头配置对象
+        headers: {
+            Authorization: localStorage.getItem('token') || ''
+        },
+        success: function (res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败')
+            } else {
+                // 调用 renderAvatar 渲染用户的头像
+                renderAvatar(res.data)
+            }
+        },
+    });
+}
+
+//渲染用户头像
+function renderAvatar(user) {
+    //1.获取用户的名称
+    var name = user.nickname || user.username
+    //2.设置欢迎的文本格式
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+    //渲染用户头像
+    if (!!user.user_pic) {
+        //3.1 渲染图片头像
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+}
+
+
+
